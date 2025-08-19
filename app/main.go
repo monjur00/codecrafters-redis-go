e2d32db1e86259e6323ed5351afcdb3a47787316
Monjure Alam <malamsk@vendasta.com>
2025-08-19 16:19:29 +0530

    muti ping

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -21,11 +22,34 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	con, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer l.Close()
 
-	con.Write([]byte("+PONG\r\n"))
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handlerConn(con)
+	}
+}
+
+func handlerConn(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			fmt.Println("Error reading connection: ", err.Error())
+		}
+
+		fmt.Printf("received %d bytes\n", n)
+		fmt.Println("received the following data:", string(buf[:n]))
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
