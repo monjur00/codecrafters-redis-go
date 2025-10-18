875847a3f4054f7033b353aa59df24040cfb762c
Monjure Alam <malamsk@vendasta.com>
2025-10-18 10:19:15 +0530

    expiry time added

--- app/main.go
+++ app/main.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type resp struct {
@@ -16,30 +17,91 @@ type resp struct {
 	args []string
 }
 
+type valueS struct {
+	value    string
+	expireAt *time.Time
+}
+
 type store struct {
 	mu   sync.RWMutex
-	data map[string]string
+	data map[string]*valueS
 }
 
 func newStore() *store {
 	return &store{
-		data: make(map[string]string),
+		data: make(map[string]*valueS),
 	}
 }
 
-func (s *store) set(key, value string) {
+func (s *store) set(key, value string, ops ...string) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.data[key] = value
+	fmt.Printf("ops %v\n", ops)
+
+	if len(ops) == 0 {
+		s.data[key] = &valueS{
+			value: value,
+		}
+		return nil
+	}
+
+	switch strings.ToLower(ops[0]) {
+	case "ex": // set expiry in second
+		if len(ops) != 2 {
+			return fmt.Errorf("missing expiry time")
+		}
+		durationS, err := strconv.Atoi(ops[1])
+		if err != nil {
+			return err
+		}
+		expiry := time.Now().Add(time.Duration(durationS) * time.Second)
+		s.data[key] = &valueS{
+			value:    value,
+			expireAt: &expiry,
+		}
+		return nil
+	case "px": // set expiry in milisecond
+		if len(ops) != 2 {
+			return fmt.Errorf("missing expiry time")
+		}
+		durationMS, err := strconv.Atoi(ops[1])
+		if err != nil {
+			return err
+		}
+		expiry := time.Now().Add(time.Duration(durationMS) * time.Millisecond)
+		s.data[key] = &valueS{
+			value:    value,
+			expireAt: &expiry,
+		}
+		return nil
+
+	default:
+		return fmt.Errorf("invalid cmd '%s'", ops[0])
+	}
 }
 
 func (s *store) get(key string) (string, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	value, ok := s.data[key]
-	return value, ok
+	v, ok := s.data[key]
+	if !ok {
+		return "", ok
+	}
+
+	fmt.Printf("Retrieve data %v", v)
+
+	if v.expireAt == nil {
+		return v.value, true
+	}
+
+	now := time.Now()
+	if v.expireAt.After(now) {
+		return v.value, true
+	} else {
+		return "", false
+	}
 }
 
 func main() {
@@ -90,7 +152,19 @@ func (s *store) handlerConn(conn net.Conn) {
 				fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(r.args[0]), r.args[0])
 			}
 		case "SET":
-			s.set(r.args[0], r.args[1])
+			if len(r.args) > 2 {
+				err := s.set(r.args[0], r.args[1], r.args[2:]...)
+				if err != nil {
+					errR := fmt.Sprintf("-ERR %s\r\n", err.Error())
+					conn.Write([]byte(errR))
+				}
+			} else {
+				err := s.set(r.args[0], r.args[1])
+				if err != nil {
+					errR := fmt.Sprintf("-ERR %s\r\n", err.Error())
+					conn.Write([]byte(errR))
+				}
+			}
 			conn.Write([]byte("+OK\r\n"))
 		case "GET":
 			if value, ok := s.get(r.args[0]); ok {
